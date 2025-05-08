84f1aab7798e552de0d43aa75d38634553e2178c
Prilaz <prilazshrestha090@gmail.com>
2025-05-08 16:51:17 +0545

    Added mynetwokpage

--- cloneapp/src/MyRoutes.jsx
+++ cloneapp/src/MyRoutes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import Home from "./Pages/Home";
+import MyNetwork from "./Pages/MyNetwork";
 
 const MyRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const MyRoutes = () => {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path="/MyNetwork" element={<MyNetwork />} />
           </Route>
         </Routes>
       </BrowserRouter>
